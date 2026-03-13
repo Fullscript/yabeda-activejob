50d0b3edff18a1a06971d08ba5143b3dcb84634d
Janos Laszlo Vasik <janosl.vasik@gmail.com>
2026-03-13 09:35:26 +0100

    Refactor handle_perform to use common_labels
    * Utilize #common_labels in EventHandler#handle_perform

--- lib/yabeda/activejob/event_handler.rb
+++ lib/yabeda/activejob/event_handler.rb
@@ -8,11 +8,8 @@ module Yabeda
       end
 
       def handle_perform
-        labels = {
-          activejob: event.payload[:job].class.to_s,
-          queue: event.payload[:job].queue_name.to_s,
-          executions: event.payload[:job].executions.to_s,
-        }
+        labels = common_labels(event.payload[:job])
+
         if event.payload[:exception].present?
           Yabeda.activejob_failed_total.increment(
             labels.merge(failure_reason: event.payload[:exception].first.to_s),
